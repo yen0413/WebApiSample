1d28c2c237c92d104a21a20104160e2b7d2c4d54
yen0413 <ct102ct102ct102@gmail.com>
2024-08-22 13:44:27 +0800

    project dotnet version upgrade net6 to net 8

--- WebApiSample/IService/ICustomerService.cs
+++ WebApiSample/IService/ICustomerService.cs
@@ -6,6 +6,7 @@ namespace WebApiSample.IService
     {
         Task<List<Customers>> GetAllCustomers();
         Task<Customers> GetCustomerByID(string CustomerID);
+        Task<Customers> GetCustomerByIDWithResults(string CustomerID);
         Task<int> UpdateCustomerByID(Customers _Customer);
         Task<int> DeleteCustomerByID(string CustomerID);
         Task<int> PostCustomer(Customers customers);
